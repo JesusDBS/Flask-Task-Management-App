13791e2ecf0609d24eac9c8763c462eff3b3e759
JesusDBS <jesusbingucv@gmail.com>
2022-06-01 09:18:12 -0400

    [ADD] flask-bootstrap

--- task_app.py
+++ task_app.py
@@ -1,7 +1,10 @@
 from flask import Flask
 from flask import make_response, render_template, redirect, url_for
+from flask_bootstrap import Bootstrap
+
 
 app = Flask(__name__)
+bootstrap = Bootstrap(app)
 
 @app.route('/')
 def index():
