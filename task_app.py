2bd903a2bc004872503ce88a2df1f1108828e346
JesusDBS <jesusbingucv@gmail.com>
2022-06-01 23:42:49 -0400

    [ADD] TaskForm to todos route

--- task_app.py
+++ task_app.py
@@ -14,6 +14,10 @@ class NameForm(FlaskForm):
     name = StringField("What's your name?", validators=[DataRequired()])
     submit = SubmitField("Submit")
 
+class TaskForm(FlaskForm):
+    todos = StringField("Insert your tasks separate by blank space", validators=[DataRequired()])
+    submit = SubmitField("Submit")
+
 @app.errorhandler(404)
 def page_not_found(e):
     return render_template("404.html"), 404
@@ -30,18 +34,26 @@ def index():
         if old_name is not None and old_name != form.name.data:
             flash("You changed your name!")
         session['name'] = form.name.data 
-        
+
         return redirect(url_for('index'))
     return render_template('index.html', form=form,
         name=session.get('name'))
 
-@app.route('/todos/<todos>')
-def todos(todos):
-    tasks_list = [todo for todo in todos.split(' ') if todo]
+@app.route('/todos', methods=['GET', 'POST'])
+def todos():
+    form = TaskForm()
+    name = session.get('name')
+    context = {
+            'form': form,
+            'name': name,
+            'todos': session.get('task_list')
+    }
 
-    if len(tasks_list) >= 1:
+    if form.validate_on_submit():
+        session['task_list'] = [todo for todo in form.todos.data.split(' ') if todo]
         
-        return render_template('todos.html', todos=tasks_list)
+        return redirect(url_for('todos'))    
+    return render_template('todos.html', **context)
 
 @app.route('/done')
 def done():
